95ff5b662e32636e3ecf9769ff8486aa007d95c1
maximus009 <kssrisingavenger@gmail.com>
2017-12-08 02:09:02 -0500

    adding optical flow

---
+++ optical/__init__.py
@@ -0,0 +1,12 @@
+## Optical Flow
+# 2-stream CNN
+# FlowNet
+
+import os
+base_path = os.getcwd()
+if not os.path.basename(base_path) == 'moviescope':
+    base_path = os.path.join(base_path, '..')
+
+import sys
+sys.path.append(base_path)
+
