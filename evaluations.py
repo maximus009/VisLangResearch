5e5e8fc34175b69b225e8cbe64fad507e3f1d0b1
maximus009 <kssrisingavenger@gmail.com>
2017-12-12 21:53:30 -0500

    Test for optical flow

--- evaluations.py
+++ evaluations.py
@@ -1,10 +1,12 @@
 ## write the evaluation functions
-
-
 from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import average_precision_score
 import os
 import numpy as np
+
+from optical.optical_models import spatio_temporal_model
+from optical.optical_utils import get_blocks
+
 
 base_path = os.getcwd()
 if not os.path.basename(base_path) == 'moviescope':
@@ -63,13 +65,51 @@ def infer_fastVideo(_id, featureName='res', model=None):
             input_feature.append(avg)
         input_feature = np.array(input_feature)
 
+    del featuresPath 
+
     pred = model.predict(input_feature)
     if _multiple:
         return pred
     else:
         return pred[0]
 
-##def infer_flownet(_id, featureName='res', model=None):
+def infer_flownet(_id, model=None):
+    if type(_id) == list:
+        _multiple = True
+    else:
+        _multiple = False
+
+    if model is None:
+        print "No model passed."
+        return
+    elif type(model) == str:
+        # always passing a weights file
+        model = load_model(model)
+    else:
+        pass
+    xs, xf = [], []
+    if not _multiple:
+        image, flow, _ = get_blocks(_id)
+        xs.append(image)
+        xf.append(flow)
+    else:
+        for block in map(get_blocks, _id):
+            image = block[0]
+            flow = block[1]
+            xs.append(image)
+            xf.append(flow)
+    xs = np.array(xs)
+    xf = np.array(xf)
+
+    input_feature = [xs, xf]
+    pred = model.predict(input_feature)
+    model.save('model_flow.h5')
+
+    if _multiple:
+        return pred
+    else:
+        return pred[0]
+
 
 def predict_genres_movie(config):
     ID = config['id']
--- test_models.py
+++ test_models.py
@@ -1,5 +1,7 @@
 import os
-from evaluations import infer_fastVideo, find_precision_recall
+from time import time
+
+from evaluations import infer_flownet, infer_fastVideo, find_precision_recall
 from utils import get_ids, get_labels
 
 trainIds, valIds, testIds = get_ids()
@@ -11,16 +13,19 @@ if not os.path.basename(base_path) == 'moviescope':
 
 def test_fastVideo_single(_id=999):
     modelSavePath = os.path.join(base_path,'trained_models','video','model_average.h5')
-    print infer_fastVideo(_id, model=modelSavePath)
+    return infer_fastVideo(_id, model=modelSavePath)
 
 def test_fastVideo_set(ids, labels, limit=None):
     modelSavePath = os.path.join(base_path,'trained_models','video','model_average.h5')
-    from time import time
     s = time()
     preds = infer_fastVideo(ids[:limit], model=modelSavePath)
     print time()-s,'seconds'
     p, r, a = find_precision_recall(labels[:limit], preds)
     print a
 
-def test_flownet(ids, labels, limit=None):
+def test_flownet_single(_id=999):
+    modelSavePath = os.path.join(base_path, 'trained_models', 'video', 'model_flow.h5')
+    return infer_flownet(_id, modelSavePath)
 
+def test_flownet_set(ids, labels, limit=None):
+    ## this goes here
