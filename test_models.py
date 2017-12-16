fd10e5e923804509f507895eea5efee06efc9641
maximus009 <kssrisingavenger@gmail.com>
2017-12-15 20:18:45 -0500

    Late fusion Bi-modal

--- test_models.py
+++ test_models.py
@@ -21,7 +21,7 @@ def test_fastVideo_set(ids, labels, limit=None):
     preds = infer_fastVideo(ids[:limit], model=modelSavePath)
     print time()-s,'seconds'
     p, r, a = find_precision_recall(labels[:limit], preds)
-    print a
+    return preds, a
 
 def test_flownet_single(_id=999):
     modelSavePath = os.path.join(base_path, 'trained_models', 'video', 'model_flow.h5')
@@ -34,7 +34,7 @@ def test_flownet_set(ids, labels, limit=None):
     preds = infer_flownet(ids[:limit], model=modelSavePath)
     print time()-s,'seconds'
     p, r, a = find_precision_recall(labels[:limit], preds)
-    print a
+    return preds, a
 
 def test_fastText_single(_id=999):
     weightsPath = os.path.join(base_path, 'trained_weights', 'text', 'best_weights_glove.h5')
@@ -46,6 +46,64 @@ def test_fastText_set(ids, labels, limit=None):
     preds = infer_fastText(ids[:limit], weightsPath)
     print time()-s,"seconds"
     p, r, a = find_precision_recall(labels[:limit], preds)
+    return preds, a
+
+def late_fusion(load=True):
+    from keras.layers import Input, TimeDistributed, Dense, Flatten
+    from keras.models import Model, load_model
+    from keras.optimizers import SGD
+    import numpy as np
+
+    train_limit = 2000
+    val_limit = None
+    vis_preds_train,_ = test_fastVideo_set(trainIds, trainLabels, limit=train_limit)
+    text_preds_train,_ = test_fastText_set(trainIds, trainLabels, limit=train_limit)
+
+    vis_preds_val,_ = test_fastVideo_set(valIds, valLabels, limit=val_limit)
+    text_preds_val,_  = test_fastText_set(valIds, valLabels, limit=val_limit)
+
+    train_input = np.dstack((vis_preds_train, text_preds_train))
+    val_input = np.dstack((vis_preds_val, text_preds_val))
+
+    #train_input = np.swapaxes(train_input, 1,2)
+    #val_input = np.swapaxes(val_input, 1,2)
+
+    if not load:
+        input_layer = Input(shape=(13,2))
+        out = Dense(13, activation='softmax', use_bias=False)(input_layer)
+        out = Flatten()(Dense(1, activation='sigmoid', use_bias=False)(out))
+        model = Model(inputs=input_layer, outputs=out)
+        model.summary()
+    else:
+        model = load_model("model_vislang_late_2.h5")
+
+    model.compile(loss='binary_crossentropy', optimizer=SGD(lr=1e-3, decay=1e-2, nesterov=True, momentum=0.8))
+    model.fit(train_input, trainLabels[:train_limit], validation_data=(val_input, valLabels[:val_limit]), epochs=200)
+    model.save('model_vislang_late_2.h5')
+    model.save_weights('weights_vislang_late_2.h5')
+
+def test_late_fusion():
+    import numpy as np
+    from keras.models import load_model
+
+    vis_pred_test, _ = test_fastVideo_set(testIds, testLabels)
+    text_pred_test, _ = test_fastText_set(testIds, testLabels)
+    test_input = np.dstack((vis_pred_test, text_pred_test))
+
+    model = load_model('model_vislang_late_2.h5')
+    preds = model.predict(test_input)
+
+    p, r, a = find_precision_recall(testLabels, preds)
     return a
 
-print test_fastText_set(testIds, testLabels)
+def modal_attention():
+    from keras.models import load_model
+    model = load_model('model_vislang_late_2.h5')
+    genre_layer = model.layers[1].get_weights()[0].T
+    print genre_layer
+    #for l in model.layers:
+    #    print l.get_weights()
+
+late_fusion()
+#print test_late_fusion()
+modal_attention()
