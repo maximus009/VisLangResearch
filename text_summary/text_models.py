e927a9f8d2ed5c6add3ece28405b54aa5dc2f2a1
maximus009 <kssrisingavenger@gmail.com>
2017-12-15 20:19:15 -0500

    Provision to include top

--- text_summary/text_models.py
+++ text_summary/text_models.py
@@ -3,12 +3,15 @@ from keras.layers import Input, Embedding, GlobalAveragePooling1D, Dense
 from keras.models import Model
 
 
-def fastText_model(l):
+def fastText_model(l=200, return_top=True):
     input_layer = Input(shape=(l,), dtype='int32')
     embedding = Embedding(50001, 300, input_length=l)
     embed_seq = embedding(input_layer)
     fastText = GlobalAveragePooling1D()(embed_seq)
-    dense_1 = Dense(64, activation='relu')(fastText)
-    prediction = Dense(13, activation='sigmoid')(dense_1)
-    model = Model(inputs=input_layer, outputs=prediction)
+    out_feature = Dense(64, activation='relu')(fastText)
+    if return_top:
+        out_feature = Dense(13, activation='sigmoid')(out_feature)
+
+    model = Model(inputs=input_layer, outputs=out_feature)
+
     return model
